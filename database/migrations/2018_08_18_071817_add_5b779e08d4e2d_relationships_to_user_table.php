9b069a45ab99a16594000fe7fa69985dde624903
danushka96 <danushkaherath96@gmail.com>
2018-08-25 22:20:29 +0530

    initial repo files

---
+++ database/migrations/2018_08_18_071817_add_5b779e08d4e2d_relationships_to_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Add5b779e08d4e2dRelationshipsToUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function(Blueprint $table) {
+            if (!Schema::hasColumn('users', 'role_id')) {
+                $table->integer('role_id')->unsigned()->nullable();
+                $table->foreign('role_id', '197961_5b779e08573d2')->references('id')->on('roles')->onDelete('cascade');
+                }
+                
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function(Blueprint $table) {
+            
+        });
+    }
+}
---
+++ database/seeds/UserSeed.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $items = [
+            
+            ['id' => 1, 'name' => 'Admin', 'email' => 'admin@admin.com', 'password' => '$2y$10$DHKFHDI0EMIRoJzCohQbTOFfBKstTRGSo7lI61IQbSu8Vniyh3rCi', 'role_id' => 1, 'remember_token' => '',],
+
+        ];
+
+        foreach ($items as $item) {
+            \App\User::create($item);
+        }
+    }
+}
