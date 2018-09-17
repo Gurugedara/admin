fc3a096bf6eb62540c15a36139001a5f04f34ab5
Lahiru Madushan <lahiru.mirihagoda@gmail.com>
2018-09-17 10:20:48 +0530

    made a change to Gurugedara href-> /

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -339,7 +339,7 @@ span.psw {
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>                        
       </button>
-      <a class="navbar-brand" href="index.html">Gurugedara</a>
+      <a class="navbar-brand" href="/">Gurugedara</a>
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav navbar-right">
