d44369f71c30842afb7f94c0cab5f6e0384ada48
Danushka Herath <Danushkaherath96@gmail.com>
2018-10-14 10:50:58 +0530

    add layout format for the views

--- app/Http/Controllers/dummyPathController.php
+++ app/Http/Controllers/dummyPathController.php
@@ -10,10 +10,11 @@ class dummyPathController extends Controller
         switch ($path){
             case "dashboard": return view('admin.dashboard'); break;
             case "studentVerify": return view('admin.student'); break;
-            case "form":return view('admin.forms'); break;
+            case "forms":return view('admin.forms'); break;
             case "calendar": return view('admin.calendar');break;
             case "forum": return view('admin.forum'); break;
-            case "chat": return view('admin.profile'); break;
+            case "chat": return view('admin.chat'); break;
+            case "profile": return view('admin.profile'); break;
             case "timeline": return view('admin.timeline'); break;
             default: return view('admin.404');
         }
