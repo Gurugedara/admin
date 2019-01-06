f56b5738584982890e55f75fd790bc6a8c528521
Lmadushan <lahiruthebest1@gmail.com>
2019-01-06 12:33:27 +0530

    created result window

---
+++ resources/views/AppSearchview/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.mcq')
+@section('content')
+
+<br>
+<br>
+<br>
+
+<div class="container col-xs-12">
+    <div class=" select-part-section p-2 rounded" style="margin-top:50px">
+        <form method="POST" action="/app/searched">
+        @csrf
+            <div class="row">
+                <div class="col-lg-9">
+                    <select name="courseId" class="form-control form-control-lg" placeholder="select language">
+                        @foreach($courses as $course)
+                        <option value="{{$course->id}}" >{{$course->name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <br>
+                <div class="col-lg-3">
+                <input type="submit" class="btn btn-primary btn-block search-btn" value="Search Institute"><i class="fa fa-search" aria-hidden="true"></i>
+                </div>
+            </form>
+        </div>
+     </div>
+   </div>
+</div>
+
+@endsection
